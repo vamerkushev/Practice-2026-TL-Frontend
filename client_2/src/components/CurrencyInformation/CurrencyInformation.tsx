6fafeb1637eda05f2aed0241dcf52529810ed6f9
Вячеслав Меркушев <vamerkushev@yandex.ru>
2026-08-28 05:59:03 +0300

    Client_2hw: обновил блок More about, добавил скрытие и показ описаний валют по нажатию кнопки, корректное отображение пары в кнопке и контент добавлен еще ранее при подключении локальных мок-данных

--- client_2/src/components/CurrencyInformation/CurrencyInformation.tsx
+++ client_2/src/components/CurrencyInformation/CurrencyInformation.tsx
@@ -2,6 +2,7 @@ import styles from './CurrencyInformation.module.scss';
 import ArrowTop from '../../images/ArrowTop.png';
 import { Button } from '../Button/Button';
 import type { Currency } from '../../types/currency';
+import { useState } from 'react';
 
 type CurrencyInformationProps = {
   fromCurrency: Currency;
@@ -9,28 +10,32 @@ type CurrencyInformationProps = {
 };
 
 export const CurrencyInformation = ({ fromCurrency, toCurrency }: CurrencyInformationProps) => {
+  const [isShown, setIsShown] = useState(false);
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <Button kind="default" className={styles.button}>
+        <Button kind="default" className={styles.button} onClick={() => setIsShown((value) => !value)}>
           <span>
             {fromCurrency.code}/{toCurrency.code}: about{' '}
           </span>
-          <img className={styles.arrow} src={ArrowTop} alt="arrow-top"></img>
+          <img className={`${styles.arrow} ${!isShown && styles[`arrow-closed`]}`} src={ArrowTop} alt="arrow-top"></img>
         </Button>
         <span className={styles.line}></span>
       </div>
-      <div className={styles.block}>
-        <h2 className={styles.title}>
-          {fromCurrency.name} - {fromCurrency.code} - {fromCurrency.symbol}
-        </h2>
-        <p className={styles.text}>{fromCurrency.description}</p>
-      </div>
-      <div className={styles.block}>
-        <h2 className={styles.title}>
-          {toCurrency.name} - {toCurrency.code} - {toCurrency.symbol}
-        </h2>
-        <p className={styles.text}>{toCurrency.description}</p>
+      <div className={`${styles.content} ${!isShown && styles[`content-hidden`]}`}>
+        <div className={`${styles.block} ${!isShown && styles.none}`}>
+          <h2 className={styles.title}>
+            {fromCurrency.name} - {fromCurrency.code} - {fromCurrency.symbol}
+          </h2>
+          <p className={styles.text}>{fromCurrency.description || 'No description!'}</p>
+        </div>
+        <div className={`${styles.block} ${!isShown && styles.none}`}>
+          <h2 className={styles.title}>
+            {toCurrency.name} - {toCurrency.code} - {toCurrency.symbol}
+          </h2>
+          <p className={styles.text}>{toCurrency.description || 'No description!'}</p>
+        </div>
       </div>
     </div>
   );
